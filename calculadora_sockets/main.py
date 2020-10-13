4d232312d7b3132be044623674dfcbaedb1865f6
JESGOD <yonnyes@unicauca.edu.co>
2020-10-13 17:49:26 -0500

    error cliente ingresa str arreglado

--- calculadora_sockets/main.py
+++ calculadora_sockets/main.py
@@ -8,30 +8,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketCliente:
 
     # intercambio de informacion
 
-    # -resibe menu desde el servidor y lo imprime
-    respuestaMenu = socketCliente.recv(1024).decode()
-    print(respuestaMenu,"\n")
-    # resibe desde teclado la opcion de menu y la envia
-    envio = input().encode()
-    socketCliente.sendall(envio)
+    # resibe menu desde el servidor y lo imprime
+    while True:
+        respuestaMenu = socketCliente.recv(1024).decode()
+        if not respuestaMenu:
+            break
+        print(respuestaMenu,"\n")
+        # resibe desde teclado la opcion de menu y la envia
+        envioM = input().encode()
+        socketCliente.sendall(envioM)
 
-    # intercambio de datos para las operacioens
-
-
-    # resibe (ingrese el primer numero) - imprime el mensaje y envia el dato
-    respuestaNum1 = socketCliente.recv(1024).decode()
-    print(respuestaNum1)
-    n1 = input()
-    socketCliente.sendall(n1.encode())
-    # resibe (ingrese el segundo numero) - imprime el mensaje y envia el dato
-    respuestaNum2 = socketCliente.recv(1024).decode()
-    print(respuestaNum2)
-    n2 = input()
-    socketCliente.sendall(n2.encode())
-
-    # resibe el resultado y lo muestra
-    respuesta = socketCliente.recv(1024).decode()
-    print(respuesta, "\n" )
-    respuestaResult = socketCliente.recv(1024).decode()
-    print("          ",respuestaResult)
 
--- calculadora_sockets/socketServer.py
+++ calculadora_sockets/socketServer.py
@@ -8,92 +8,49 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketServer:
     conexion, direccion = socketServer.accept()
    # with conexion:
     print('conexion establecida desde :', direccion)
-    #while True:
-    conexion.sendall("  por favor ingrese una opcion:\n"
-                         "          1: sumar \n"
-                         "          2: restar \n"
-                         "          3: multiplicar \n"
-                         "          4: dividir \n"
-                         "          5: salir \n".encode())
-    datosR = conexion.recv(1024).decode()
-    parseDatosStr = str(datosR)
-    parseDatosInt = int(parseDatosStr)
-
-       # if not datosR:
-        #    break
-
-    if parseDatosInt == 1:
-        bandera = False
-        while bandera == False:
+    parseDatosInt = 1
+    while parseDatosInt != 5:
+        conexion.sendall("  por favor ingrese una opcion:\n"
+                             "          1: sumar \n"
+                             "          2: restar \n"
+                             "          3: multiplicar \n"
+                             "          4: dividir \n"
+                             "          5: salir \n".encode())
+        bandera = True
+        while bandera == True:
             try:
-                conexion.sendall(" ingrese el primer numero ".encode())
-                n1 = conexion.recv(1024).decode()
-                parsen1Str = str(n1)
-                print(n1)
-                parsen1Int = int(parsen1Str)
-
-                conexion.sendall(" ingrese el segundo numero ".encode())
-                n2 = conexion.recv(1024).decode()
-                parsen2Str = str(n2)
-                print(parsen2Str)
-                parsen2Int = int(parsen2Str)
-                # print(parsen1Str, "", parsen2Str)
-                resultadoS = parsen1Int + parsen2Int
-                resultadoSCod = str(resultadoS)
-                conexion.sendall("***** el resultado de la suma es*****".encode())
-                conexion.sendall(resultadoSCod.encode())
-                bandera = True
+                datosR = conexion.recv(1024).decode()
+                parseDatosStr = str(datosR)
+                print(parseDatosStr)
+                parseDatosInt = int(parseDatosStr)
+                bandera = False
             except ValueError:
-                print("\n********** ERRO: por favor ingrese un valor correcto **********\n")
-    elif parseDatosInt == 2:
-        bandera = False
-        while bandera == False:
-            try:
-                conexion.sendall(" ingrese el primer numero ".encode())
-                n1 = conexion.recv(1024).decode()
-                parsen1Str = str(n1)
-                print(n1)
-                parsen1Int = int(parsen1Str)
+                #control = "1"
+                #conexion.sendall(control.encode())
+                conexion.sendall("ingrese un valor correcro para el menu".encode())
+                parsen1Int = 0
 
-                conexion.sendall(" ingrese el segundo numero ".encode())
-                n2 = conexion.recv(1024).decode()
-                parsen2Str = str(n2)
-                print(parsen2Str)
-                parsen2Int = int(parsen2Str)
-                # print(parsen1Str, "", parsen2Str)
-                resultadoS = parsen1Int - parsen2Int
-                resultadoSCod = str(resultadoS)
-                conexion.sendall("***** el resultado de la suma es*****".encode())
-                conexion.sendall(resultadoSCod.encode())
-                bandera = True
-            except ValueError:
-                print("\n********** ERRO: por favor ingrese un valor correcto **********\n")
+        if parseDatosInt == 1:
+            bandera = False
+            while bandera == False:
+                try:
+                    conexion.sendall(" ingrese el primer numero ".encode())
+                    n1 = conexion.recv(1024).decode()
+                    parsen1Str = str(n1)
+                    parsen1Int = int(parsen1Str)
 
-    elif parseDatosInt == 3:
-        bandera = False
-        while bandera == False:
-            try:
-                conexion.sendall(" ingrese el primer numero ".encode())
-                n1 = conexion.recv(1024).decode()
-                parsen1Str = str(n1)
-                print(n1)
-                parsen1Int = int(parsen1Str)
-
-                conexion.sendall(" ingrese el segundo numero ".encode())
-                n2 = conexion.recv(1024).decode()
-                parsen2Str = str(n2)
-                print(parsen2Str)
-                parsen2Int = int(parsen2Str)
-                # print(parsen1Str, "", parsen2Str)
-                resultadoS = parsen1Int * parsen2Int
-                resultadoSCod = str(resultadoS)
-                conexion.sendall("***** el resultado de la suma es*****".encode())
-                conexion.sendall(resultadoSCod.encode())
-                bandera = True
-            except ValueError:
-                print("\n********** ERRO: por favor ingrese un valor correcto **********\n")
-    else:
-        if parseDatosInt == 4:
+                    conexion.sendall(" ingrese el segundo numero ".encode())
+                    n2 = conexion.recv(1024).decode()
+                    parsen2Str = str(n2)
+                    parsen2Int = int(parsen2Str)
+                    resultadoS = parsen1Int + parsen2Int
+                    resultadoSCod = str(resultadoS)
+                   # conexion.sendall("***** el resultado de la suma es*****".encode())
+                    conexion.sendall(resultadoSCod.encode())
+                    bandera = True
+                except ValueError:
+                    print("\n********** ERRO: por favor ingrese un valor correcto **********\n")
+        elif parseDatosInt == 2:
             bandera = False
             while bandera == False:
                 try:
@@ -109,15 +66,63 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketServer:
                     print(parsen2Str)
                     parsen2Int = int(parsen2Str)
                     # print(parsen1Str, "", parsen2Str)
-                    resultadoS = parsen1Int / parsen2Int
+                    resultadoS = parsen1Int - parsen2Int
                     resultadoSCod = str(resultadoS)
-                    conexion.sendall("***** el resultado de la suma es*****".encode())
+                    #conexion.sendall("***** el resultado de la resta es*****".encode())
                     conexion.sendall(resultadoSCod.encode())
                     bandera = True
                 except ValueError:
                     print("\n********** ERRO: por favor ingrese un valor correcto **********\n")
-                except ZeroDivisionError:
-                    print("\n********** ERRO: no se puede dividir por cero **********\n")
+
+        elif parseDatosInt == 3:
+            bandera = False
+            while bandera == False:
+                try:
+                    conexion.sendall(" ingrese el primer numero ".encode())
+                    n1 = conexion.recv(1024).decode()
+                    parsen1Str = str(n1)
+                    print(n1)
+                    parsen1Int = int(parsen1Str)
+
+                    conexion.sendall(" ingrese el segundo numero ".encode())
+                    n2 = conexion.recv(1024).decode()
+                    parsen2Str = str(n2)
+                    print(parsen2Str)
+                    parsen2Int = int(parsen2Str)
+                    # print(parsen1Str, "", parsen2Str)
+                    resultadoS = parsen1Int * parsen2Int
+                    resultadoSCod = str(resultadoS)
+                   # conexion.sendall("***** el resultado de la multiplicacion es*****".encode())
+                    conexion.sendall(resultadoSCod.encode())
+                    bandera = True
+                except ValueError:
+                    print("\n********** ERRO: por favor ingrese un valor correcto **********\n")
+        else:
+            if parseDatosInt == 4:
+                bandera = False
+                while bandera == False:
+                    try:
+                        conexion.sendall(" ingrese el primer numero ".encode())
+                        n1 = conexion.recv(1024).decode()
+                        parsen1Str = str(n1)
+                        print(n1)
+                        parsen1Int = int(parsen1Str)
+
+                        conexion.sendall(" ingrese el segundo numero ".encode())
+                        n2 = conexion.recv(1024).decode()
+                        parsen2Str = str(n2)
+                        print(parsen2Str)
+                        parsen2Int = int(parsen2Str)
+                        # print(parsen1Str, "", parsen2Str)
+                        resultadoS = parsen1Int / parsen2Int
+                        resultadoSCod = str(resultadoS)
+                       # conexion.sendall("***** el resultado de la division es*****".encode())
+                        conexion.sendall(resultadoSCod.encode())
+                        bandera = True
+                    except ValueError:
+                        print("\n********** ERRO: por favor ingrese un valor correcto **********\n")
+                    except ZeroDivisionError:
+                        print("\n********** ERRO: no se puede dividir por cero **********\n")
 
 
 
